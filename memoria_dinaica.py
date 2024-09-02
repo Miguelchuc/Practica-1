56b674800d73ebc22e555024a1c905e4a9d55e5e
TedeLimonn <Limonmike14@gmail.com>
2024-09-02 12:39:09 -0600

    Add files via upload

---
+++ memoria_dinaica.py
@@ -0,0 +1,7 @@
+frutas = ["Mango", "Manzana", "Banana", "Uvas"]
+print(frutas)
+
+frutas.pop(3)  
+frutas.pop(0)  
+frutas.append("sandia")
+print(frutas)
---
+++ memoria_estatica.py
@@ -0,0 +1,6 @@
+cali = [0]*5 
+
+for i in range (5): 
+    cali[i]=int(input("Ingrese las calificaciones: "))
+
+print("Las calificaciones son: ", cali)
